bd79a4961f65af0ebc5c3f73fe354c687b03b448
Vitória Luísa da Silva <vitoria.luisa@indicium.tech>
2025-08-28 17:48:56 +0000

    Ajustes

--- models/intermediate/int_salesreason__enriched.sql
+++ models/intermediate/int_salesreason__enriched.sql
@@ -1,17 +1,25 @@
 with
 
-salesreason as (
+order_reason as (
+    select *
+    from {{ ref('stg_erp__salesorderheadersalesreason') }}
+),
+
+reason as (
     select *
     from {{ ref('stg_erp__salesreason') }}
 ),
 
-cleaned as (
+joined as (
     select
-        salesreason_pk
-        , salesreason_name
-        , salesreason_type
-    from salesreason
+        orr.order_fk,
+        r.reason_pk,
+        r.reason_name,
+        r.reason_type
+    from order_reason orr
+    left join reason r
+        on orr.reason_fk = r.reason_pk
 )
 
 select *
-from cleaned
+from joined
